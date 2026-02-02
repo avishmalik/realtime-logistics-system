e2d85e0aaeb4d5e89c76de02fea57675b783c5b2
Avish Malik <avishmalik@Avishs-MacBook-Air.local>
2026-02-02 22:34:28 +0530

    Add FastAPI order service with Kafka integration and Redis status lookup

---
+++ services/order_api/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from kafka import KafkaProducer
+import redis
+import json
+import uuid
+
+app = FastAPI()
+
+# Kafka Producer
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+# Redis
+r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+
+@app.post("/orders")
+def create_order():
+
+    order_id = str(uuid.uuid4())[:8]
+
+    event = {
+        "order_id": order_id,
+        "status": "PLACED"
+    }
+
+    producer.send("orders", event)
+    producer.flush()
+
+    return {
+        "message": "Order created",
+        "order_id": order_id
+    }
+
+
+@app.get("/orders/{order_id}")
+def get_order(order_id: str):
+
+    status = r.get(f"order:{order_id}")
+
+    if not status:
+        return {"error": "Order not found"}
+
+    return {
+        "order_id": order_id,
+        "status": status
+    }
